135a93bf0c6e37ad088d1d0dee16d74be7612d3a
Victor Tolesano <victor.tolesano@semantix.com.br>
2019-04-10 10:52:17 -0300

    criado o progressBar do layout de cadastro

--- HerbalistApp/app/src/main/java/com/br/herbalistapp/MainActivity.kt
+++ HerbalistApp/app/src/main/java/com/br/herbalistapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.br.herbalistapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.widget.*
 import kotlinx.android.synthetic.main.login.*
--- HerbalistApp/app/src/main/java/com/br/herbalistapp/TelaCadastroActivity.kt
+++ HerbalistApp/app/src/main/java/com/br/herbalistapp/TelaCadastroActivity.kt
@@ -3,6 +3,10 @@ package com.br.herbalistapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.cadastro.*
 import kotlinx.android.synthetic.main.login.*
@@ -13,8 +17,24 @@ class TelaCadastroActivity : DebugActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cadastro)
 
-        botao_enviar_cadastro.setOnClickListener {
-            Toast.makeText(this,"cadastro enviado", Toast.LENGTH_SHORT).show()
-        }
+//        botao_enviar_cadastro.setOnClickListener {
+//            Toast.makeText(this,"cadastro enviado", Toast.LENGTH_SHORT).show()
+//        }
+
+//        var fl: FrameLayout = FrameLayout(this)
+//
+//        var param : FrameLayout.LayoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+//                                                                        FrameLayout.LayoutParams.MATCH_PARENT )
+//        val text_view: TextView = TextView(this)
+//        text_view.layoutParams = param
+//        fl.layoutParams = param
+//        text_view.setText("LOADING")
+//        fl.addView(text_view)
+//        setContentView(fl)
+    }
+
+    fun enviarCadastro(view: View){
+        val fl:FrameLayout = findViewById(R.id.frame_layout_cadastrar)
+        fl.visibility = view.visibility
     }
 }
