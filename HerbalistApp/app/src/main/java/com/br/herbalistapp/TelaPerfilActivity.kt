7fc4f088ce859298b6100d9d6147f278cdf4c874
Victor Tolesano <victor.tolesano@semantix.com.br>
2019-04-14 15:41:23 -0300

    criado nova activity DrawerLayoutActivity e seus layouts

--- HerbalistApp/app/src/main/java/com/br/herbalistapp/TelaPerfilActivity.kt
+++ HerbalistApp/app/src/main/java/com/br/herbalistapp/TelaPerfilActivity.kt
@@ -1,9 +1,7 @@
 package com.br.herbalistapp
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -13,9 +11,7 @@ class TelaPerfilActivity : DebugActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.perfil)
-//        var params = intent.extras
-//        val nome = params.get("name")
-//        Toast.makeText(this,"${nome}", Toast.LENGTH_SHORT).show()
+
 
     }
 
