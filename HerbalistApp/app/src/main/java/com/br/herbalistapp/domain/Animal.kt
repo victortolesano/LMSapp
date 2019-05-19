f46a75ba1f2e04b0d1343f5ab90defa44a5943b6
Victor Tolesano <victor.tolesano@semantix.com.br>
2019-05-19 09:00:04 -0300

    concertado os conflitos de dependencia

--- HerbalistApp/app/src/main/java/com/br/herbalistapp/domain/Animal.kt
+++ HerbalistApp/app/src/main/java/com/br/herbalistapp/domain/Animal.kt
@@ -3,4 +3,4 @@ package com.br.herbalistapp.domain
 import android.graphics.Picture
 import java.io.Serializable
 
-class Animal(val name: String, val description: String, val pictureUrl: String) : Serializable;
+class Animal(val name: String, val description: String, val pictureUrl: String) : Serializable
