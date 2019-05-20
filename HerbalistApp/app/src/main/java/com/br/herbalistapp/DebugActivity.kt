42f2be1d4e3b2849d2df489767a39e66ce2a0416
Victor Tolesano <victor.tolesano@semantix.com.br>
2019-05-20 19:07:06 -0300

    arrumando conflito

--- HerbalistApp/app/src/main/java/com/br/herbalistapp/DebugActivity.kt
+++ HerbalistApp/app/src/main/java/com/br/herbalistapp/DebugActivity.kt
@@ -3,6 +3,7 @@ package com.br.herbalistapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
 open class DebugActivity : AppCompatActivity() {
 
@@ -46,4 +47,6 @@ open class DebugActivity : AppCompatActivity() {
         super.onDestroy()
         Log.d(TAG,"${clasName} onDestroy chamado")
     }
+
+    open fun onClick(v: View?) {}
 }
